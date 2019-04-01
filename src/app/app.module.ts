7bd7c2cad1a587f2789a55b0e7c173c5b4c5f8c5
IrinaSiandetskaya <irene_siv@inbox.ru>
2019-04-01 15:48:53 +0300

    add validation in login page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {Routes, RouterModule} from '@angular/router';
+import { FormsModule }   from '@angular/forms';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { CoursesComponent } from './courses/courses.component';
 import { AddCourseComponent } from './add-course/add-course.component';
 import { EditCourseComponent } from './edit-course/edit-course.component';
 
+const appRoutes: Routes =[
+  { path: 'login', component: LoginComponent},
+  { path: '', redirectTo: 'login', pathMatch:'full'},
+  { path: 'courses', component: CoursesComponent},
+  { path: 'courses/new', component: AddCourseComponent },
+  { path: 'courses/:id', component: EditCourseComponent }
+];
 @NgModule({
   declarations: [
     AppComponent,
@@ -16,7 +24,7 @@ import { EditCourseComponent } from './edit-course/edit-course.component';
     EditCourseComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, FormsModule,RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,15 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { NgModel } from "@angular/forms";
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  selector: "app-login",
+  templateUrl: "./login.component.html",
+  styleUrls: ["./login.component.css"]
 })
 export class LoginComponent implements OnInit {
+  constructor() {}
+  user: User = new User();
 
-  constructor() { }
-
-  ngOnInit() {
+  addUser() {
+    console.log(this.user);
   }
 
+  checkUser() {
+    if (this.user.login === "q" && this.user.password === "q") {
+      console.log(
+        "Check user \nlogin: " +
+          this.user.login +
+          " password: " +
+          this.user.password
+      );
+    } else {
+      console.log("Check user:incorrect " + this.user);
+    }
+  }
+
+  ngOnInit() {}
+}
+
+export class User {
+  login: string;
+  password: string;
 }
