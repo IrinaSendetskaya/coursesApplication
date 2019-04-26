9117be04346cae44ef8bfd445a33138cc00bc364
IrinaSiandetskaya <irene_siv@inbox.ru>
2019-04-26 17:24:11 +0300

    change date format

--- src/app/datepicker/datepicker.component.ts
+++ src/app/datepicker/datepicker.component.ts
@@ -1,19 +1,18 @@
-import { Component } from "@angular/core";
+import { Component, Input, OnChanges, SimpleChanges, SimpleChange } from "@angular/core";
 import { FormControl } from "@angular/forms";
-import {MomentDateAdapter} from '@angular/material-moment-adapter';
-import {DateAdapter, MAT_DATE_FORMATS} from '@angular/material/core';
+import { MomentDateAdapter } from "@angular/material-moment-adapter";
+import { DateAdapter, MAT_DATE_FORMATS } from "@angular/material/core";
 
-import * as _moment from 'moment';
-import {defaultFormat as _rollupMoment} from 'moment';
+import * as moment from 'moment';
+import { defaultFormat as _rollupMoment } from "moment";
 
-const moment = _rollupMoment || _moment;
 
 export const MY_FORMATS = {
   parse: {
     dateInput: "LL"
   },
   display: {
-    dateInput: "DD.MM.YYYY",
+    dateInput: "DD.MM.YYYY"
   }
 };
 
@@ -21,9 +20,18 @@ export const MY_FORMATS = {
   selector: "app-datepicker",
   templateUrl: "./datepicker.component.html",
   styleUrls: ["./datepicker.component.css"],
-  providers: [{provide: DateAdapter, useClass: MomentDateAdapter},
-  {provide: MAT_DATE_FORMATS, useValue: MY_FORMATS},]
+  providers: [
+    {provide: DateAdapter, useClass: MomentDateAdapter},
+    {provide: MAT_DATE_FORMATS, useValue: MY_FORMATS},
+  ]
 })
-export class DatepickerComponent {
-  date = new FormControl(moment);
+export class DatepickerComponent implements OnChanges{
+
+  @Input() dateForParent;
+
+  date = new FormControl(moment());
+
+ngOnChanges(changes: { [propName: string]: SimpleChange }){
+if(changes['dateForParent'])console.log(changes);
+}
 }
